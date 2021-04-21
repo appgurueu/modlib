7e00e3a2375a4a8f8f75bff9bc0359883d1a2182
Lars Mueller <appgurulars@gmx.de>
2021-04-21 21:29:22 +0200

    Tabs

--- init.lua
+++ init.lua
@@ -78,7 +78,7 @@ end
 local load_module, get_resource, loadfile_exports
 modlib = setmetatable({
 	-- TODO bump on release
-	version = 64,
+	version = 65,
 	modname = minetest and minetest.get_current_modname(),
 	dir_delim = rawget(_G, "DIR_DELIM") or "/",
 	_RG = setmetatable({}, {
